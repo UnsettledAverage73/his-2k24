c75c554eee701321df6110c16c4ac0e5bc0308b1
0xguava <demo.lappy@protonmail.com>
2024-11-26 17:36:07 +0530

    done partially remediation

---
+++ ubuntu/v22.04/initial_setup/Configure Additional Process Hardening/caph-rem.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+installed-rem(){
+  echo -e "\n- Audit for \"$@ installed check\":"
+  if [[ -n "$(dpkg-query -s $@ 2> /dev/null)" ]]; then
+    apt purge $@ -y &> /dev/null
+    echo -e "\t- Remediation: **SUCCESS**"
+  else 
+    echo -e "\t- Remediation: Everything is **OK**"
+  fi
+}
+
+pkgs=('prelink' 'apport')
+for pkg in "${pkgs[@]}";do 
+  installed-rem "$pkg"
+done
